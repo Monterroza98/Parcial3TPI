909d61b20b16bc5f929f070da43868ee8357b3ad
jokerkr98 <kejoromo1998@gmail.com>
2018-04-28 01:53:38 -0600

    initial commit

---
+++ ManttoWebApp/ManttoWebApp-web/src/main/java/sv/uesocc/edu/ingenieria/tpi135_2018/mantto/shobeselacommit.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.uesocc.edu.ingenieria.tpi135_2018.mantto;
+
+/**
+ *
+ * @author joker
+ */
+public class shobeselacommit {
+    
+}
